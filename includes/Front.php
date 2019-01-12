d135d139fefc7fcdc36ea6fe5552da683be39057
Mehrshad Darzi <mehrshad198@gmail.com>
2019-01-12 19:55:06 +0330

    add option

--- includes/Front.php
+++ includes/Front.php
@@ -62,31 +62,34 @@ class Front {
 			'url'   => ''
 		);
 
-		//Setup Data
-		$user_id = get_current_user_id();
-		$time    = date( "Y-m-d H:i:s", $_REQUEST['time'] );
+		if ( isset( $_GET['wp_online_pub_check_notification'] ) and isset( $_GET['time'] ) and is_user_logged_in() ) {
 
-		//check New Alert Ticke
-		$alert = $wpdb->get_row( "SELECT COUNT(*) FROM `z_ticket` WHERE `user_id` = {$user_id} and `sender` = 'admin' and `read_user` =0" );
-		if ( null !== $alert ) {
-			$url = add_query_arg( array( 'order' => $alert['chat_id'] ), get_the_permalink( WP_Online_Pub::$option['user_panel'] ) );
-			$result = array(
-				'exist' => 'yes',
-				'title' => 'پیام جدید',
-				'text'  => 'شما یک پیام جدید دارید',
-				'url'   => $url
-			);
-		}
+			//Setup Data
+			$user_id = get_current_user_id();
+			$time    = date( "Y-m-d H:i:s", $_REQUEST['time'] );
 
-		//Check New Factor
-		$factor = $wpdb->get_row( "SELECT * FROM `z_factor` WHERE `user_id` = $user_id AND `date` >= $time ORDER BY `id` DESC LIMIT 1", ARRAY_A );
-		if ( null !== $factor ) {
-			$result = array(
-				'exist' => 'yes',
-				'title' => 'فاکتور جدید',
-				'text'  => 'شما یک فاکتور دارید',
-				'url'   => add_query_arg( array( 'view_factor' => $factor['id'], 'redirect' => 'user', '_security_code' => wp_create_nonce( 'view_factor_access' ) ), home_url() )
-			);
+			//check New Alert Ticket
+			$alert = $wpdb->get_row( "SELECT * FROM `z_ticket` WHERE `user_id` = {$user_id} and `sender` = 'admin' and `read_user` =0", ARRAY_A );
+			if ( null !== $alert ) {
+				$result = array(
+					'exist' => 'yes',
+					'title' => 'پیام جدید',
+					'text'  => 'شما یک پیام جدید دارید',
+					'url'   => add_query_arg( array( 'order' => $alert['chat_id'] ), get_the_permalink( WP_Online_Pub::$option['user_panel'] ) )
+				);
+			}
+
+			//Check New Factor
+			$factor = $wpdb->get_row( "SELECT * FROM `z_factor` WHERE `user_id` = $user_id AND `date` >= $time ORDER BY `id` DESC LIMIT 1", ARRAY_A );
+			if ( null !== $factor ) {
+				$result = array(
+					'exist' => 'yes',
+					'title' => 'فاکتور جدید',
+					'text'  => 'شما یک فاکتور دارید',
+					'url'   => add_query_arg( array( 'view_factor' => $factor['id'], 'redirect' => 'user', '_security_code' => wp_create_nonce( 'view_factor_access' ) ), home_url() )
+				);
+			}
+
 		}
 
 		//Send Data
